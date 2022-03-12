2021b56ef7b2ffb1e37ac4743ad102b33712fa67
Guilherme Macedo <99128432+guilhermemma@users.noreply.github.com>
2022-03-12 13:53:45 -0300

    Criação do JS

---
+++ scripts/game.js
@@ -0,0 +1,14 @@
+let bord = ['','','','','','','','','']
+let playerTime = 0
+let symbols = ['o','x']
+
+
+function handleMove(position){
+    bord[position] = symbols[playerTime]
+
+    if(playerTime == 0){
+         playerTime = 1;
+    }else{
+        playerTime = 0;
+    }
+}
